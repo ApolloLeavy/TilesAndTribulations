9204d20128ac9e48f5e1401037389b4df1c7bd67
Awesomesnowman <justindao33@gmail.com>
2024-04-11 15:10:56 -0400

    prefabs

---
+++ Assets/Players/Knight/Fortify.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NETWORK_ENGINE;
+public class Fortify : Projectile
+{
+
+    Player p;
+    public override void HandleMessage(string flag, string value)
+    {
+
+    }
+
+    public override void NetworkedStart()
+    {
+        StartCoroutine(Timer());
+    }
+
+    public override IEnumerator SlowUpdate()
+    {
+        
+        yield return new WaitForSeconds(.1f);
+
+    }
+    public IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(1);
+        MyCore.NetDestroyObject(NetId);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
